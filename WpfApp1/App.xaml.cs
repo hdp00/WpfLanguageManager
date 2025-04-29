f1b9ded666334dccf4ae648eb15ce5a718ba58d4
hdp <hdp00@126.com>
2025-04-29 16:31:25 +0800

    测试代码

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -5,13 +5,18 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using DevExpress.Xpf.Core;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// App.xaml 的交互逻辑
-    /// </summary>
+    /// <summary>  
+    /// App.xaml 的交互逻辑  
+    /// </summary>  
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ApplicationThemeHelper.ApplicationThemeName = "Office2019Black";
+        }
     }
 }
