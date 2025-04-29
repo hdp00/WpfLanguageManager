a9ec21fcb8cb0f70659703f9e189e05c2a1b5436
hdp <hdp00@126.com>
2025-04-29 16:40:51 +0800

    测试代码

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace WpfApp1
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : ThemedWindow
     {
         public MainWindow()
         {
